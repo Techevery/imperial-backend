5245fdff19bfded26e7b432d803f8ef9c25b6d0d
Tmahn001 <toluiwalewa@gmail.com>
2022-10-05 07:59:46 -0400

    collectstatic and remote requirements installation

--- config/settings.py
+++ config/settings.py
@@ -165,8 +165,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/techwwpp/imperial.techevery.ng/media'
+STATIC_ROOT = '/home/techwwpp/imperial.techevery.ng/static'
